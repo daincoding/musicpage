d2d6d22c8ca7112de93a9bce28e65c21f962fb63
daincoding <official.toyou@gmail.com>
2025-01-23 11:07:47 +0100

    Filled in Content and Smooth Scrolling

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
 })
 );
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+    });
+  });
